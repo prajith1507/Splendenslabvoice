506d4ba51df2231aefc727ffc3c588d3529a3ac5
prajith1507 <prajithkumarit@gmail.com>
2025-06-13 22:09:01 -0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -69,7 +69,7 @@ const Footer = () => {
         </div>
 
         <div className="border-t border-slate-800 mt-8 pt-8 text-center text-slate-400 text-sm">
-          <p>&copy; 2024 Splendenslab IT Ventures Pvt Ltd. All rights reserved.</p>
+          <p>&copy; 2025 Splendenslab IT Ventures Pvt Ltd. All rights reserved.</p>
         </div>
       </div>
     </footer>
